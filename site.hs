098421250ad9009386bfa14093d9b12d77cb84dc
jonathanrlouie <jonathanrlouie@gmail.com>
2025-03-10 11:01:29 -0700

    Fix broken images and add spacer

--- site.hs
+++ site.hs
@@ -7,7 +7,7 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "Assets/**" $ do
         route   idRoute
         compile copyFileCompiler
 
